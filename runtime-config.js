ce0a85bff6816d2c609e1d21c4c1d842cac5fb72
Barbara Chacin <barbara.asucar@gmail.com>
2026-09-18 13:37:10 -0400

    config: set Drive import endpoint

--- runtime-config.js
+++ runtime-config.js
@@ -1,5 +1,5 @@
 // Versioned public runtime settings. Leave the endpoint empty to disable Drive imports.
 window.ORTHOGRAPHY_RUNTIME_CONFIG = Object.freeze({
   version: 1,
-  driveImportEndpoint: '',
+  driveImportEndpoint: 'https://corrector-ortografico-drive-import.barbara-asucar.workers.dev/import',
 });
